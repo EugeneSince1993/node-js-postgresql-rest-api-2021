845395912e64666dae46f9f20a8ad9dfef71c4b3
EugeneSince1993 <jk235@mail.ru>
2022-04-19 20:58:14 +0300

    making user.routes.js

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const Pool = require('pg').Pool;
-const pool = new Pool();
+const pool = new Pool({
+  user: "postgres",
+  password: "Fullstack2022",
+  host: "localhost",
+  port: 5432,
+  database: ""
+});
 
 module.exports = pool;
-// 3.40
